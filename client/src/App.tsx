2f212eaba4936568bd3728e951cbcd87f07cf3f0
joaopdr0sena <joao@DESKTOP-90N2D5C>
2023-09-28 18:00:21 -0300

    :package: (client) fix & update: app & main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,11 @@
-import { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-
-const socket = io('localhost:3000', { transports: ['websocket'] });
-socket.emit('joinRoom', { username: 'joão', room: 'cinema' })
+import AppRoutes from './routes';
+import GlobalStyle from './styles/global';
 
 export default function App() {
-  const [clientMessage, setClientMesssage] = useState('');
-  const [serverMessages, setServerMassages] = useState<string[]>([])
-
-  useEffect(() => {
-    const handleMessages = (newMessage: string) => (
-      setServerMassages([...serverMessages, newMessage])
-    );
-    socket.on('serverMessage', handleMessages)
-  }, [serverMessages])
-
-  const handleSendMassage = (message: string) => {
-    socket.emit('roomClientMessage', {
-      room: 'cinema',
-      message: message,
-    });
-    setClientMesssage('');
-  }
-
   return (
-    <div>
-      <input type="text" value={clientMessage} onChange={({ target }) => setClientMesssage(target.value)} />
-      <button type="submit" onClick={() => handleSendMassage(clientMessage)}>submit</button>
-      <ul>
-        {serverMessages && serverMessages.map((message) =>(
-          <li>{message}</li>
-        ))}
-      </ul>
-    </div>
-  )
+    <>
+      <AppRoutes />
+      <GlobalStyle />
+    </>
+  );
 }
