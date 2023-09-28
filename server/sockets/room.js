b02b8e8bc3f4af1e885719500730e307da6cc9e7
joaopdr0sena <joao@DESKTOP-90N2D5C>
2023-09-28 17:53:10 -0300

    :bug: (server) fix: socket emit in room

--- server/sockets/room.js
+++ server/sockets/room.js
@@ -1,13 +1,18 @@
 module.exports = (io) => io.on('connection', (socket) => {
   socket.on('joinRoom', ({username, room}) => {
     socket.join(room);
-  
+
     socket.emit('serverMessage', `Boas vindas ${username} a sala sobre ${room}`);
   
     socket.broadcast.emit('serverMessage', `${username} acabou de entrar na sala`);
     
     socket.on('roomClientMessage', ({message, room}) => {
-      io.to(room).emit('serverMessage', `${username}: ${message}`);
+      io.to(room).emit('comunitMessage', {
+        id: socket.id,
+        username,
+        message,
+        date: new Date().toISOString(),
+      });
     });
   });
 });
