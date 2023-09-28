31ed7a6c5269376acee79c064c74daf2fc89bd51
joaopdr0sena <joao@DESKTOP-90N2D5C>
2023-09-28 17:53:56 -0300

    :lipstick: (client) create: global style

---
+++ client/src/styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
