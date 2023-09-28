f0626d6209d62935393c68622bdbd7272c0d9f0f
joaopdr0sena <joao@DESKTOP-90N2D5C>
2023-09-28 17:50:33 -0300

    :bug: fix: eslint rules

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -2,17 +2,23 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/prefer-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
 }
