6ae285963acc0061afe95ad48421ee88c5b0a836
joaopdr0sena <joao@DESKTOP-90N2D5C>
2023-09-28 17:48:21 -0300

    :fire: (server) remove: chat.js

--- server/index.js
+++ server/index.js
@@ -2,13 +2,10 @@ const express = require('express');
 const http = require('http');
 
 const ioServer = require('socket.io');
-// const chatSocket = require('./sockets/chat');
 const roomSocket = require('./sockets/room')
 
 const app = express();
 const server = http.createServer(app);
-
-// app.use(express.static(__dirname + '/public'));
 
 const io = ioServer(server, {
   cors: {
