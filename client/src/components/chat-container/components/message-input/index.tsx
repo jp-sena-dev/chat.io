c375e0b94a7622c957aa6964c8fc121e697c238f
joaopdr0sena <joao@DESKTOP-90N2D5C>
2023-12-07 07:42:45 -0300

    :sparkles: (client) create: message input

---
+++ client/src/components/chat-container/components/message-input/index.tsx
@@ -0,0 +1,40 @@
+import { Box, IconButton, InputBase } from '@mui/material';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import SendIcon from '@mui/icons-material/Send';
+
+interface InputMessageProps {
+  message: string;
+  setMessage: (_pram: string) => void;
+  HandleSubmit: (_pram: string) => void;
+}
+
+export function MessageInput({ message, setMessage, HandleSubmit }: InputMessageProps) {
+  return (
+    <Box
+      component="form"
+      onSubmit={(e) => e.preventDefault()}
+      sx={{
+        p: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        height: '95%',
+        border: '1px solid #d7d7d7',
+        borderRadius: '50px',
+      }}
+    >
+      <Box sx={{ p: '10px' }}>
+        <AttachFileIcon />
+      </Box>
+      <InputBase
+        value={message}
+        onChange={({ target }) => setMessage(target.value)}
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Type message"
+      />
+      <IconButton type="submit" onClick={() => HandleSubmit(message)} sx={{ p: '10px' }}>
+        <SendIcon />
+      </IconButton>
+    </Box>
+  );
+}
