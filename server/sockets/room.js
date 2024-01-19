eed7657b660474caf3844d4e557b92448763c4cc
joaopedr0sena <joaopedrosena1997@gmail.com>
2024-01-19 19:55:16 -0300

    :sparkles: (server) create: room update event

--- server/sockets/room.js
+++ server/sockets/room.js
@@ -1,14 +1,12 @@
 module.exports = (io) => io.on('connection', (socket) => {
+  socket.on('updateRoom', (room) => {
+    socket.broadcast.emit('updateRoom', room);
+  })
+
   socket.on('joinRoom', ({username, userId, room}) => {
     socket.removeAllListeners('roomClientMessage');
     socket.join(room);
      
-
-    socket.on('updateRoom', (room) => {
-      io.to(room).emit('updateRoom');
-    })
-    
-
     socket.on('roomClientMessage', ({message, room}) => {
       io.to(room).emit('comunitMessage', {
         id: userId,
