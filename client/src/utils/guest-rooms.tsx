c1b6a8d59a1b406bcca769b5a9c6e0674a469e40
joaopedr0sena <joaopedrosena1997@gmail.com>
2024-02-16 12:21:38 -0300

    :lipstick: (client) update: guest rooms images

--- client/src/utils/guest-rooms.tsx
+++ client/src/utils/guest-rooms.tsx
@@ -10,26 +10,31 @@ export const guetRooms: RoomCollection[] = [
   {
     id: 'movie',
     name: 'Movie',
+    imageURL: '/rooms/movies.jpg',
     users: [guestUser, guestUser, guestUser],
   },
   {
     id: 'sport',
     name: 'Sport',
+    imageURL: 'rooms/sports.jpg',
     users: [guestUser, guestUser, guestUser],
   },
   {
     id: 'technology',
     name: 'Technology',
+    imageURL: 'rooms/technology.jpg',
     users: [guestUser, guestUser, guestUser],
   },
   {
     id: 'music',
     name: 'Music',
+    imageURL: 'rooms/music.jpg',
     users: [guestUser, guestUser, guestUser],
   },
   {
     id: 'memes',
     name: 'Memes',
+    imageURL: 'rooms/memes.jpg',
     users: [guestUser, guestUser, guestUser],
   },
 ];
